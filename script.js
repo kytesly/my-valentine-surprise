a042794205c929af082d40661f624d5f60886eff
kytesly <bighead634251@gmail.com>
2026-02-01 06:03:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const noButton = document.getElementById('no-button');
 
-noButton.addEventListener('mouseover', () => {
+function moveButton() {
     const randomX = Math.random() * (window.innerWidth - noButton.offsetWidth);
     const randomY = Math.random() * (window.innerHeight - noButton.offsetHeight);
     noButton.style.position = 'absolute';
     noButton.style.left = randomX + 'px';
     noButton.style.top = randomY + 'px';
-});
+}
+
+noButton.addEventListener('mouseover', moveButton);
+noButton.addEventListener('touchstart', moveButton);
